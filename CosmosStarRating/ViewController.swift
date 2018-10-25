8e6fbcbf02dd67baff651d210ce2a49669e02732
parth-padhariya <parth.padhariya@xongolab.com>
2018-10-25 16:56:33 +0530

    Initial Commit

---
+++ CosmosStarRating/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CosmosStarRating
+//
+//  Created by MacMiniOld on 25/10/18.
+//  Copyright © 2018 Xongolab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
